a07f585038ac65e43a0822a07925e505308838e7
Jake J Ursetta <jake.j.ursetta@lmco.com>
2018-03-15 17:20:54 -0600

    Refs #11 and #12 - Comments Cleaned up

--- app/models/ProjectModel.js
+++ app/models/ProjectModel.js
@@ -21,15 +21,16 @@
  */
 
 
-// Allocate database credentials
-// TODO (JU) - Implement a system to pull database info from a configuration file
+// Requirements
 const mongoose = require('mongoose')
 
+// Declare database credentials
+// TODO (JU) - Implement a system to pull database info from a configuration file
 const url = 'mongodb://localhost:27017/';
 const dbName = 'mms';
 
 
-// Connect to Data basee
+// Connect to Data base
 mongoose.connect(url + dbName);
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
@@ -38,8 +39,8 @@ db.on('error', console.error.bind(console, 'connection error:'));
 // Create Project Model Schema:
 var Schema = mongoose.Schema;
 
-// _id      = Primary Key
-// name     = Name of project for VE
+// id       = Primary Key
+// name     = Name of project
 // projects = Organization the project belongs to referenced from the
 //            Organization Model
 var ProjectSchema = new Schema({
