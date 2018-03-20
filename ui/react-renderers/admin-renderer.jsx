2e2187ff59d141000caf7905a705bb8550b11126
Josh Kaplan <joshua.d.kaplan@lmco.com>
2018-03-20 13:46:28 -0400

    Refs #000 - Renaming react folders

---
+++ ui/react-renderers/admin-renderer.jsx
@@ -0,0 +1,23 @@
+/*****************************************************************************
+ * Classification: UNCLASSIFIED                                              *
+ *                                                                           *
+ * Copyright (C) 2018, Lockheed Martin Corporation                           *
+ *                                                                           *
+ * LMPI WARNING: This file is Lockheed Martin Proprietary Information.       *
+ * It is not approved for public release or redistribution.                  *
+ *                                                                           *
+ * EXPORT CONTROL WARNING: This software may be subject to applicable export *
+ * control laws. Contact legal and export compliance prior to distribution.  *
+ *****************************************************************************/
+/*
+ * admin-renderer.jsx
+ *
+ * Josh Kaplan <joshua.d.kaplan@lmco.com>
+ *
+ * This renders the admin console.
+ */
+
+ReactDOM.render(
+  <AdminConsole name="MBEE-Admin"/>,
+  document.getElementById('root')
+);
