ba44b1083587dc2c2d384e5cca622055d7be06c5
Jake J Ursetta <jake.j.ursetta@lmco.com>
2018-06-07 14:34:09 -0600

    Added refactored code for creating orgs

--- app/controllers/OrganizationController.js
+++ app/controllers/OrganizationController.js
@@ -60,7 +60,7 @@ class OrganizationController {
    * @param  {String} The string containing the username of the requesting user.
    * @param  {String} The string of the org ID.
    */
-  static getOrg(username, orgid) {
+  static findOrg(username, orgid) {
     return new Promise(function(resolve, reject) {
       const orgId = M.lib.sani.sanitize(orgid);
       Organization.findOne({ id: orgId }, (err, org) => {
@@ -89,6 +89,75 @@ class OrganizationController {
 
     })
   }
+
+  /**
+   * This function takes a user and org id and creates a new organization.
+   *
+   * @example
+   * OrganizationController.createOrg('josh', {mbee-sw})
+   * .then(function(org) {
+   *   // do something with the newly createdorg
+   * })
+   * .catch(function(error) {
+   *   M.log.error(error);
+   * });
+   *
+   *
+   * @param  {User} The object containing the user of the requesting user.
+   * @param  {id: (string), name: (string)} The JSON of the new org.
+   */
+  static createOrg(user, org) {
+    return new Promise(function(resolve, reject) {
+
+      // Error check - Make sure user is admin
+      if (!user.admin){
+        return reject(new Error('User cannot create orgs.'))
+      }
+
+      // Error check - Make sure organization body has an organization id and name
+      if (!org.hasOwnProperty('id')) {
+        return reject(new Error('Organization ID not included.'));
+      }
+      if (!org.hasOwnProperty('name')) {
+        return reject(new Error('Organization does not have a name.'));
+      }
+
+      // Sanitize fields
+      const orgId = M.lib.sani.html(org.id);
+      const orgName = M.lib.sani.html(org.name);
+
+      // Error check - Make sure a valid orgid and name is given
+      if (!RegExp('^([a-z])([a-z0-9-]){0,}$').test(orgId)) {
+        return reject(new Error('Organization ID is not valid.'));
+      }
+      if (!RegExp('^([a-zA-Z0-9-\\s])+$').test(orgName)) {
+        return reject(new Error('Organization name is not valid.'));
+      }
+
+      Organization.find({ id: orgId }, (err, orgs) => {
+        // If error, log it and return error
+        if (err) {
+          return reject(err);
+        }
+        // If org already exists, throw an error
+        if (orgs.length >= 1) {
+          return reject(new Error('Organization already exists.'));
+        }
+        
+        // Create the new org and save it
+        const newOrg = new Organization({
+          id: orgId,
+          name: orgName
+        });
+        newOrg.save();
+
+        // Return the response message
+        return resolve(newOrg);
+      });
+    })
+  }
+
+
 
 }
 
