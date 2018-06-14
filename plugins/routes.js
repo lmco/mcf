2c359e1cb8b37f1541b065c5aae8cbf607a8d131
e371904 <matthew.w.kline@lmco.com>
2018-06-14 13:12:24 -0600

    update

--- plugins/routes.js
+++ plugins/routes.js
@@ -73,10 +73,11 @@ files.forEach((f) => {
   const stdout = execSync(commands.join('; '));
   M.log.verbose(stdout.toString());
 
-  console.log("Could not load plugin");
-
   // Install the plugin
-  pluginRouter.use(`/${namespace}`, require(entrypoint)); // eslint-disable-line global-require
+  pluginRouter.use(`/${namespace}`, require(entrypoint)).catch(function(){
+    console.log("Could not load plugin");
+    return;
+  }); // eslint-disable-line global-require
   M.log.info(`Plugin ${namespace} installed.`);
 });
 
