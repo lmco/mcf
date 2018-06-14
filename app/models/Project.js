de6c5de506ce50cc5f23c1837fdbe9fb468c04e2
Josh Kaplan <joshua.d.kaplan@lmco.com>
2018-06-14 10:54:31 -0400

    Added renamed models. Deleted role controller.

---
+++ app/models/Project.js
@@ -0,0 +1,135 @@
+/*****************************************************************************
+ * Classification: UNCLASSIFIED                                              *
+ *                                                                           *
+ * Copyright (C) 2018, Lockheed Martin Corporation                           *
+ *                                                                           *
+ * LMPI WARNING: This file is Lockheed Martin Proprietary Information.       *
+ * It is not approved for public release or redistribution.                  *
+ *                                                                           *
+ * EXPORT CONTROL WARNING: This software may be subject to applicable export *
+ * control laws. Contact legal and export compliance prior to distribution.  *
+ *****************************************************************************/
+/**
+ * ProjectsModel.js
+ *
+ * Jake Ursetta <jake.j.ursetta@lmco.com>
+ *
+ * The ProjectModel.js file creates a mongoose model to interact with the MongoDB
+ * Database in order to find, save, update, and delete projects.
+ */
+
+// Requirements
+const mongoose = require('mongoose');
+const path = require('path');
+const M = require(path.join(__dirname, '..', '..', 'mbee.js'));
+
+// Create Project Model Schema:
+const Schema = mongoose.Schema;
+
+// TODO (JU) - Discuss use of '_id' vs 'id'
+//
+// id       = Primary Key
+// name     = Name of project
+// projects = Organization the project belongs to referenced from the
+//            Organization Model
+const ProjectSchema = new Schema({
+  id: {
+    type: String,
+    require: true,
+    index: true,
+    match: RegExp('^([a-z])([a-z0-9-]){0,}$'),
+    maxlength: [36, 'Too many characters in username']
+  },
+
+  org: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Organization',
+    require: true
+  },
+
+  uid: {
+    type: String,
+    unique: true,
+  },
+
+  name: {
+    type: String,
+    requite: true,
+    unique: true,
+    match: RegExp('^([a-zA-Z0-9-\\s])+$')
+  },
+
+  permissions: {
+    read: [{
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User'
+    }],
+
+    write: [{
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User'
+    }],
+
+    admin: [{
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User'
+    }]
+  }
+
+
+});
+
+ProjectSchema.virtual('members').get(function() {
+  // Grab the write and admin permissions lists
+  const read  = this.permissions.read;
+  const write = this.permissions.write;
+  const admin = this.permissions.admin;
+
+  // set member to a copy of write
+  const member = read.slice();
+
+  // Add admins that aren't already in the member list,
+  // creating a unique list of members
+  for (let i = 0; i < write.length; i++) {
+    if (!member.includes(write[i])) {
+      member.push(write[i]);
+    }
+  }
+
+  for (let i = 0; i < admin.length; i++) {
+    if (!member.includes(admin[i])) {
+      member.push(admin[i]);
+    }
+  }
+
+  return member;
+});
+
+
+// Post hook to link refernece to organization upon save
+ProjectSchema.post('save', () => {
+  Organization.findOneAndUpdate({
+    _id: this.org
+  }, {                            // eslint is catching the following line by mistake, disabling
+    $push: { projects: this._id } // eslint-disable-line no-underscore-dangle
+  }, (err, org) => {
+    if (err) {
+      M.log.error(err);
+    }
+  });
+});
+
+ProjectSchema.pre('find', function() {
+  this.populate('org');
+});
+
+ProjectSchema.pre('save', function() {
+  this.populate('org');
+});
+
+// Required for virtual getters
+ProjectSchema.set('toJSON', { virtuals: true });
+ProjectSchema.set('toObject', { virtuals: true });
+
+// Export mongoose model as "Project"
+module.exports = mongoose.model('Project', ProjectSchema);
