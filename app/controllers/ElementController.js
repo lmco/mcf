01e30ceec196f42e041e5a893533e4b2000cedc7
Austin Bieber <austin.j.bieber@lmco.com>
2018-07-02 14:26:24 -0600

    Added createElement function

--- app/controllers/ElementController.js
+++ app/controllers/ElementController.js
@@ -18,7 +18,7 @@
 
 const path = require('path');
 const M = require(path.join(__dirname, '..', '..', 'mbee.js'));
-// const ProjController = M.load('controllers/ProjectController');
+const ProjController = M.load('controllers/ProjectController');
 const Element = M.load('models/Element');
 
 /**
@@ -103,6 +103,125 @@ class ElementController {
         // Return resulting element
         return resolve(element);
       });
+    });
+  }
+
+
+  /**
+   * @description  This function takes a user and element data, and creates a project.
+   *
+   * @example
+   * ElementController.findElement({Tony Stark}, {Element 1})
+   * .then(function(element) {
+   *   // do something with the element
+   * })
+   * .catch(function(error) {
+   *   M.log.error(error);
+   * });
+   *
+   *
+   * @param  {User} The user object of the requesting user.
+   * @param  {Object} The JSOn object containing the element data
+   */
+  // TODO: Handle an element parent
+  static createElement(reqUser, element) {
+    return new Promise((resolve, reject) => {
+      // Ensure element object data contains all the proper fields
+      if (!element.hasOwnProperty('id')) {
+        return reject(new Error(JSON.stringify({ status: 400, message: 'Bad Request', description: 'Element does not have attribute (id).' })));
+      }
+      if (!element.hasOwnProperty('proj')) {
+        if (!element.proj.hasOwnProperty('id')) {
+          return reject(new Error(JSON.stringify({ status: 400, message: 'Bad Request', description: 'Element does not have attribute (proj.id).' })));
+        }
+        if (!element.proj.hasOwnProperty('org')) {
+          if (!element.proj.org.hasOwnProperty('id')) {
+            return reject(new Error(JSON.stringify({ status: 400, message: 'Bad Request', description: 'Element does not have attribute (proj.org.id).' })));
+          }
+        }
+      }
+
+      // Ensure all pieces of data are strings
+      if (typeof element.id !== 'string') {
+        return reject(new Error(JSON.stringify({ status: 400, message: 'Bad Request', description: 'Element ID is not a string.' })));
+      }
+      if (typeof element.proj.id !== 'string') {
+        return reject(new Error(JSON.stringify({ status: 400, message: 'Bad Request', description: 'Project ID is not a string.' })));
+      }
+      if (typeof element.proj.org.id !== 'string') {
+        return reject(new Error(JSON.stringify({ status: 400, message: 'Bad Request', description: 'Organization ID is not a string.' })));
+      }
+      if (element.hasOwnProperty('name')) {
+        // Element name is not required, so check first if it exists.
+        if (typeof element.name !== 'string') {
+          return reject(new Error(JSON.stringify({ status: 400, message: 'Bad Request', description: 'Element name is not a string.' })));
+        }
+      }
+
+      // TODO: Check element parent for valid data
+
+      const elemID = M.lib.sani.html(element.id);
+      const projID = M.lib.sani.html(element.proj.id);
+      const orgID = M.lib.sani.html(element.proj.org.id);
+      const elemUID = `${orgID}:${projID}:${elemID}`;
+      let elemName = null;
+      if (element.hasOwnProperty('name')) {
+        elemName = M.lib.sani.html(element.name);
+      }
+
+      // Error check - make sure element ID and element name are valid
+      if (!RegExp(M.lib.validators.element.id).test(elemID)) {
+        return reject(new Error('Element ID is not valid.'));
+      }
+      if (element.hasOwnProperty('name')) {
+        if (!RegExp(M.lib.validators.element.name).test(elemName)) {
+          return reject(new Error('Element Name is not valid.'));
+        }
+      }
+
+      // Error check - make sure the project exists
+      ProjController.findProject(reqUser, orgID, projID)
+      .then((proj) => {
+        // Check Permissions
+        const writers = proj.permissions.write.map(u => u._id.toString());
+
+        if (!writers.includes(reqUser._id.toString()) && !reqUser.admin) {
+          return reject(new Error(JSON.stringify({ status: 401, message: 'Unauthorized', description: 'User does not have permission.' })));
+        }
+
+        // Error check - check if the element already exists
+        ElementController.findElement(reqUser, orgID, projID, elemID)
+        .then((elem) => reject(new Error(JSON.stringify({ status: 400, message: 'Bad Request', description: 'Element already exists.' }))))
+        .catch((error) => {
+          // This is ok, we dont want the element to already exist.
+          const err = JSON.parse(error.message);
+          if (err.description === 'Element not found.') {
+            // Create the new element
+            const newElement = new Element({
+              id: elemID,
+              name: elemName,
+              project: proj._id,
+              uid: elemUID,
+              parent: null
+            });
+
+            // Save the new element
+            newElement.save((saveErr, elementUpdated) => {
+              if (saveErr) {
+                return reject(new Error(JSON.stringify({ status: 500, message: 'Internal Server Error', description: saveErr.message })));
+              }
+
+              // Return the element if succesful
+              return resolve(elementUpdated);
+            });
+          }
+          else {
+            // Some other error, return it.
+            return reject(error);
+          }
+        });
+      })
+      .catch((error2) => reject(error2));
     });
   }
 
