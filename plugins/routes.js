1d02d5fbec27462cf936bd6de8fd9bb95b42306b
Josh Kaplan <joshua.d.kaplan@lmco.com>
2018-07-11 11:08:07 -0400

    Changes to plugin router

--- plugins/routes.js
+++ plugins/routes.js
@@ -1,18 +1,18 @@
-/*****************************************************************************
- * Classification: UNCLASSIFIED                                              *
- *                                                                           *
- * Copyright (C) 2018, Lockheed Martin Corporation                           *
- *                                                                           *
- * LMPI WARNING: This file is Lockheed Martin Proprietary Information.       *
- * It is not approved for public release or redistribution.                  *
- *                                                                           *
- * EXPORT CONTROL WARNING: This software may be subject to applicable export *
- * control laws. Contact legal and export compliance prior to distribution.  *
- *****************************************************************************/
+/******************************************************************************
+ * Classification: UNCLASSIFIED                                               *
+ *                                                                            *
+ * Copyright (C) 2018, Lockheed Martin Corporation                            *
+ *                                                                            *
+ * LMPI WARNING: This file is Lockheed Martin Proprietary Information.        *
+ * It is not approved for public release or redistribution.                   *
+ *                                                                            *
+ * EXPORT CONTROL WARNING: This software may be subject to applicable export  *
+ * control laws. Contact legal and export compliance prior to distribution.   *
+ ******************************************************************************/
 /*
- * plugin_routes.js
+ * @module plugin.routes
  *
- * Josh Kaplan <joshua.d.kaplan@lmco.com>
+ * @author Josh Kaplan <joshua.d.kaplan@lmco.com>
  *
  * This file defines the the plugin router.
  */
@@ -20,45 +20,116 @@
 const fs = require('fs');
 const path = require('path');
 const { execSync } = require('child_process');
-
 const M = require(path.join(__dirname, '..', 'mbee.js'));
-
 const express = require('express');
 const pluginRouter = express.Router();
 
-let cmd = [];
+/**
+ * Clones the plugin from a Git repository and places in the appropriate
+ * location in the plugins directory.
+ * @param data
+ */
+function clonePluginFromGitRepo(data) {
+  // TODO
+}
+
+/**
+ * Gets the plugin from a URL and places it in a specified location in the
+ * plugins directory.
+ * @param data
+ */
+function getPluginFromURL(data) {
+ // TODO
+}
+
+/**
+ * Copies the plugin from a local directory to the plugins directory.
+ * If the plugin location is already in the local directory, it should do
+ * nothing.
+ * @param data
+ */
+function copyPluginFromLocalDir(data) {
+  // TODO
+}
+
+/**
+ * Extracts a zip file into the appropriate location in the plugins directory.
+ * @param data
+ */
+function extractZip(data) {
+  // TODO
+}
+
+/**
+ * Extracts a tar.gz file into the appropriate location in the plugins
+ * directory.
+ * @param data
+ */
+function extractTarGz(data) {
+  // TODO
+}
+
+/**
+ * Extracts a gzip file into the appropriate location in the plugins directory.
+ * @param data
+ */
+function extractGz(data) {
+  // TODO
+}
+
+
+let cmd = []; // TODO - Reduce scope of this variable
 
 // Clone plugins
 for (let i = 0; i < M.config.server.plugins.plugins.length; i++) {
   const metadata = M.config.server.plugins.plugins[i];
 
   // Remove the old directory if it exists
+  // TODO  - Remove shouldn't happen if plugin is local and is in ./plugins
+  // TODO - Windows support
   let stdout = execSync(`rm -rf plugins/${metadata.name}`);
   M.log.verbose(stdout.toString());
 
   // Clone the git repository
+  // TODO - Move this
   M.log.info(`Cloning plugin ${metadata.name} from ${metadata.repository} ...`);
 
+  // TODO - Check if deploykey given
   execSync(`chmod 400 ${metadata.deployKey}`);
 
   // Determine if plugin is local or from git
-  if (metadata.repository.substr(metadata.repository.length - 4) === '.git') {
+  // TODO - Git repos don't have to end in .git. How do we want to handle this?
+  if (metadata.repository.endsWith('.git')) {
     cmd = [
       `GIT_SSH_COMMAND="ssh -i ${metadata.deployKey} -oStrictHostKeyChecking=no"`,
       `git clone ${metadata.repository} plugins/${metadata.name}`
     ].join(' ');
   }
-  else {
+  else if (metadata.repository.endsWith('.zip')) {
+    // TODO
+  }
+  else if (metadata.repository.endsWith('.tar.gz')) {
+    // TODO
+  }
+  else if (metadata.repository.endsWith('.gz')) {
+    // TODO
+  }
+  // TODO - Add support for .zip, .tar.gz, and .gz
+  // TODO - Should check that the path is a valid local path
+  else if (metadata.repository.startsWith('/') || metadata.repository.startsWith('.')) {
     cmd = [
       `cp -r ${metadata.repository} plugins/${metadata.name}`
     ].join(' ');
   }
+  else {
+    // TODO - handle unknown case
+  }
+
   // Clone the repo
   stdout = execSync(cmd);
   M.log.verbose(stdout.toString());
   M.log.info('Clone complete.');
 }
-
 
 // Load plugin routes
 const files = fs.readdirSync(__dirname);
@@ -75,6 +146,7 @@ files.forEach((f) => {
   const namespace = f.toLowerCase();
   M.log.info(`Loading plugin '${namespace}' ...`);
 
+  // TODO - Windows support
   const commands = [
     `cd ${pluginPath}`,
     'yarn install --modules-folder ../../node_modules',
