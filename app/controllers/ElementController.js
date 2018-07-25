1b7a616ce807a00436f921e29619d059441dc3f8
Bieber, Austin J <austin.j.bieber@lmco.com>
2018-07-25 13:42:47 -0600

    Allow for updating custom tags in elements, updated a test

--- app/controllers/ElementController.js
+++ app/controllers/ElementController.js
@@ -808,15 +808,25 @@ class ElementController {
           if (!validUpdateFields.includes(updateField)) {
             return reject(new errors.CustomError(`Users cannot update [${updateField}] of Elements.`, 400));
           }
+
           // Error Check - Check if updated field is of type string
-          if (typeof elementUpdated[updateField] !== 'string') {
+          if ((typeof elementUpdated[updateField] !== 'string') && (updateField !== 'custom')) {
             return reject(new errors.CustomError(`The Element [${updateField}] is not of type String`, 400));
           }
 
-          // sanitize field
-          updateVal = M.lib.sani.sanitize(elementUpdated[updateField]);
-          // Update field in element object
-          element[updateField] = updateVal;
+          // Updates each individual tag that was provided.
+          if (updateField === 'custom') {
+            // eslint-disable-next-line no-loop-func
+            Object.keys(elementUpdated[updateField]).forEach((key) => {
+              element.custom[key] = M.lib.sani.sanitize(elementUpdated[updateField][key]);
+            });
+          }
+          else {
+            // sanitize field
+            updateVal = M.lib.sani.sanitize(elementUpdated[updateField]);
+            // Update field in element object
+            element[updateField] = updateVal;
+          }
         }
 
         // Save updated element
