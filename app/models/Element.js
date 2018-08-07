e1dc4891171d9abfd5ffc109f850029880ee85dd
Bieber, Austin J <austin.j.bieber@lmco.com>
2018-08-07 10:43:34 -0600

    Generating uuid on createElement

--- app/models/Element.js
+++ app/models/Element.js
@@ -38,6 +38,7 @@
 const path = require('path');
 const mongoose = require('mongoose');
 const M = require(path.join('..', '..', 'mbee.js'));
+const uuidv4 = require('uuid/v4');
 
 // Mongoose options - for discriminators
 const options = { discriminatorKey: 'type' };
@@ -83,16 +84,20 @@ const ElementSchema = new mongoose.Schema({
     match: RegExp(M.lib.validators.element.id),
     maxlength: [36, 'Element ID is too long'],
     minlength: [2, 'Element ID is too short']
-    // default: function() {
-    //   const parts = this.uid.split(':');  // split on the ':'' separator
-    //   const id = parts[parts.length - 1]; // the last part
-    //   return id;
-    // },
-    // set: function() {
-    //   const parts = this.uid.split(':');  // split on the ':'' separator
-    //   const id = parts[parts.length - 1]; // the last part
-    //   return id;
-    // }
+  },
+
+  /**
+   * @memberOf  Element
+   * @property  {String} uuid
+   *
+   * @description  The UUID of the element. Based on RFC 4122
+   */
+  uuid: {
+    type: String,
+    required: false,
+    set: function(v) {
+      return uuidv4();
+    }
   },
 
   /**
@@ -230,6 +235,10 @@ ElementSchema.pre('save', function(next) {
   // Run our defined setters
   this.updatedOn = '';
   this.deleted = '';
+  // Only set uuid if it hasn't already been set
+  if (this.uuid === undefined) {
+    this.uuid = '';
+  }
   next();
 });
 
