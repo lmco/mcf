2152267b6355f22ffb6b346abebf3bed15273d74
Jake J Ursetta <jake.j.ursetta@lmco.com>
2018-08-21 14:25:08 -0600

    Updated lib.middleware and lib.validators

--- app/lib/middleware.js
+++ app/lib/middleware.js
@@ -1,21 +1,22 @@
-/*****************************************************************************
- * Classification: UNCLASSIFIED                                              *
- *                                                                           *
- * Copyright (C) 2018, Lockheed Martin Corporation                           *
- *                                                                           *
- * LMPI WARNING: This file is Lockheed Martin Proprietary Information.       *
- * It is not approved for public release or redistribution.                  *
- *                                                                           *
- * EXPORT CONTROL WARNING: This software may be subject to applicable export *
- * control laws. Contact legal and export compliance prior to distribution.  *
- *****************************************************************************/
 /**
+ * Classification: UNCLASSIFIED
+ *
  * @module  lib.middleware
+ *
+ * @copyright Copyright (C) 2018, Lockheed Martin Corporation
+ *
+ * @license LMPI
+ * <br/>
+ * LMPI WARNING: This file is Lockheed Martin Proprietary Information.
+ * It is not approved for public release or redistribution.<br/>
+ *
+ * EXPORT CONTROL WARNING: This software may be subject to applicable export
+ * control laws. Contact legal and export compliance prior to distribution.
  *
  * @author  Jake Ursetta <jake.j.ursetta@lmco.com>
  *
- * @description This class Defines middleware functions which can be used by
- * routes in order to implement route logging or other various functionality.
+ * @description This file defines middleware functions which can be used by
+ * express to perform actions during requests.
  */
 
 // Load MBEE modules
@@ -29,39 +30,43 @@ const errors = M.require('lib.errors');
  * @description This class Defines middleware functions which can be used by
  * routes in order to implement route logging or other various functionality.
  */
-class Middleware {
 
-  /**
-   * Log the route being accessed by a user.
-   */
-  static logRoute(req, res, next) {
-    const username = (req.user) ? req.user.username : 'anonymous';
-    M.log.info(`${req.method} "${req.originalUrl}" requested by ${username}`);
-    next();
+/**
+ * @description Log the route and method requested by a user.
+ */
+module.exports.logRoute = function logRoute(req, res, next) {
+  // Set username to anonymous if req.user is not defined
+  const username = (req.user) ? req.user.username : 'anonymous';
+  // Log the method, url, and username for the request
+  M.log.info(`${req.method} "${req.originalUrl}" requested by ${username}`);
+  next();
+};
+
+/**
+ * @description Log the IP address where the request originated from
+ */
+module.exports.logIP = function logIP(req, res, next) {
+  // Log the method, url, and ip address for the request
+  M.log.verbose(`${req.method} "${req.originalUrl}" requested from ${req.ip}`);
+  next();
+};
+
+/**
+ * @description Disables specific user api methods using the configuration
+ * server.api.userAPI
+ */
+// eslint-disable-next-line consistent-return
+module.exports.disableUserAPI = function disableUserAPI(req, res, next) {
+  // Check if the request method is disabled
+  if (!M.config.server.api.userAPI[req.method.toLowerCase()]) {
+    // Create error message '<method> <url> is disabled'
+    const message = `${req.method} ${req.originalUrl} is disabled`;
+    // Create custom error 403 Forbidden
+    const error = new errors.CustomError(message, 403);
+    // Log custom error
+    M.log.error(error);
+    // Return error to user
+    return res.status(403).send(error);
   }
-
-  /**
-   * Log ip of the request
-   */
-  static logIP(req, res, next) {
-    M.log.verbose(`${req.method} "${req.originalUrl}" requested from ${req.ip}`);
-    next();
-  }
-
-  /**
-   * Disable the user api endpoint based on the configuration file.
-   */
-  static disableUserAPI(req, res, next) { // eslint-disable-line consistent-return
-    // If the method is disabled, send back the error
-    if (!M.config.server.api.userAPI[req.method.toLowerCase()]) {
-      const message = `${req.method} ${req.originalUrl} is disabled`;
-      const error = new errors.CustomError(message, 403);
-      M.log.error(error);
-      return res.status(403).send(error);
-    }
-    next();
-  }
-
-}
-
-module.exports = Middleware;
+  next();
+};
--- app/lib/validators.js
+++ app/lib/validators.js
@@ -1,41 +1,123 @@
-/*****************************************************************************
- * Classification: UNCLASSIFIED                                              *
- *                                                                           *
- * Copyright (C) 2018, Lockheed Martin Corporation                           *
- *                                                                           *
- * LMPI WARNING: This file is Lockheed Martin Proprietary Information.       *
- * It is not approved for public release or redistribution.                  *
- *                                                                           *
- * EXPORT CONTROL WARNING: This software may be subject to applicable export *
- * control laws. Contact legal and export compliance prior to distribution.  *
- *****************************************************************************/
 /**
+ * Classification: UNCLASSIFIED
+ *
  * @module  lib.validators
+ *
+ * @copyright Copyright (C) 2018, Lockheed Martin Corporation
+ *
+ * @license LMPI
+ * <br/>
+ * LMPI WARNING: This file is Lockheed Martin Proprietary Information.
+ * It is not approved for public release or redistribution.<br/>
+ *
+ * EXPORT CONTROL WARNING: This software may be subject to applicable export
+ * control laws. Contact legal and export compliance prior to distribution.
  *
  * @author Josh Kaplan <joshua.d.kaplan@lmco.com>
  *
- * Defines common validators.
+ * @description This file defines validators - common regular expressions and
+ * helper functions - used to validate data within MBEE.
  */
 
+// TODO: include examples for each REGEXP
+
+/**
+ * @description Regular Expressions to validate organization data
+ *
+ * id:
+ *   - CANNOT include the follow reserved words: css, js, im, login, logout,
+ *     about, assets, static, public
+ *   - MUST start with a lowercase letter
+ *   - MUST only include lowercase letters, numbers or '-'
+ *   - MUST be of length 1 or more
+ *
+ * name:
+ *   - MUST start with a lowercase letter, uppercase letter, or number
+ *   - MUST ONLY include lowercase letters, uppercase letters, numbers,
+ *     '-', or whitespace
+ *   - MUST be of length 1 or more
+ *
+ */
 module.exports.org = {
-  id: '^(?!(css|js|img|login|logout|about))([a-z])([a-z0-9-]){0,}$',
-  name: '^([a-zA-Z0-9-\\s])+$'
+  id: '^(?!(css|js|img|login|logout|about|assets|static|public))([a-z])([a-z0-9-]){0,}$',
+  name: '^([a-zA-Z0-9])([a-zA-Z0-9-\\s]){0,}$'
 };
 
+/**
+ * @description Regular Expressions to validate project data
+ *
+ * id:
+ *   - MUST start with lowercase letter -> ^([a-z])
+ *   - MUST only include lowercase letters, numbers, or '-'
+ *   - Must be of length 1 or more
+ *
+ * name:
+ *   - MUST start with a lowercase letter, uppercase letter, or number
+ *   - MUST only include lowercase letters, uppercase letters, numbers,
+ *     '-', or whitespace
+ *   - MUST be of length 1 or more
+ *
+ */
 module.exports.project = {
   id: '^([a-z])([a-z0-9-]){0,}$',
-  name: '^([a-zA-Z0-9-\\s])+$'
+  name: '^([a-zA-Z0-9])([a-zA-Z0-9-\\s]){0,}$'
 };
 
+/**
+ * @description Regular Expressions to validate element data
+ *
+ * uid: TODO: ask austin where he is using this and discuss if it is necessary
+ *
+ * id: TODO: Discuss what the valid ID should be (ie: only start with lower case? stay lowercase?)
+ *   - MUST start with lowercase letter -> ^([a-z])
+ *   - MUST ONLY include lowercase letters, numbers, or '-'
+ *   - Must be of length 1 or more
+ *
+ * name:
+ *   - MUST start with a lowercase letter, uppercase letter, or number
+ *   - MUST only include lowercase letters, uppercase letters, numbers,
+ *     '-', or whitespace
+ *   - MUST be of length 1 or more
+ *
+ * uuid:
+ *   - MUST follow the following format: xxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx
+ *     where x is a number or a lowercase letter from a-f
+ *
+ *   EX: f81d4fae-7dec-11d0-a765-00a0c91e6bf6
+ *
+ */
 module.exports.element = {
   uid: '^([a-zA-Z0-9])([a-zA-Z0-9-:]){0,}$',
   id: '^([a-zA-Z0-9])([a-zA-Z0-9-]){0,}$',
-  name: '^([a-zA-Z0-9-\\s])+$',
-  uuid: '([a-zA-Z0-9]{8}(-[a-zA-Z0-9]{4}){3}-[a-zA-Z0-9]{12})'
+  name: '^([a-zA-Z0-9])([a-zA-Z0-9-\\s]){0,}$',
+  uuid: '([a-z0-9]{8}(-[a-z0-9]{4}){3}-[a-z0-9]{12})'
 };
 
+/**
+ * @description Regular Expressions to validate user data
+ *
+ * username:
+ *   - MUST start with a lowercase letter
+ *   - MUST only include lowercase letters, numbers, or underscores
+ *   - MUST be of length 1 or more
+ *
+ * password:
+ *   - MUST be of length 8 or more
+ *   - MUST contain at least 1 number
+ *   - MUST contain at lease 1 lowercase letter
+ *   - MUST contain at least 1 uppercase letter
+ *
+ * email:
+ *   - MUST be a valid email address
+ *
+ * name:
+ *   - MUST start with a lowercase letter or uppercase letter
+ *   - MUST only contain lowercase letters, uppercase letters, '-', or whitespace
+ *
+ */
 module.exports.user = {
   username: '^([a-z])([a-z0-9_]){0,}$',
+  // TODO: enforce use of special characters
   password: function(p) {
     // Error check - Make sure password is a string
     if (typeof (p) !== typeof ('')) {
@@ -58,15 +140,22 @@ module.exports.user = {
                 && uppercaseValidator));
     }
     catch (error) {
-      M.log.error(error);
+      // Explicitly NOT logging error to avoid password logging
       return false;
     }
   },
   email: '^([a-zA-Z0-9_\\-\\.]+)@([a-zA-Z0-9_\\-\\.]+)\\.([a-zA-Z]{2,5})$',
-  name: '^([a-zA-Z])+([-a-zA-Z ])*$'
+  name: '^([a-zA-Z])([-a-zA-Z ])*{0,}$'
 };
 
+/**
+ * @description Regular Expressions to validate url data
+ *
+ * next:
+ *   - MUST start with one and only one '/'
+ *
+ */
 module.exports.url = {
-  // starts with one and only one
+  // starts with one and only one '/'
   next: '^(\/)(?!\/)' // eslint-disable-line no-useless-escape
 };
