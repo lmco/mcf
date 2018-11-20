5f93c9686a50425d2e905ea7f9ddb05d717852d5
Jake J Ursetta <jake.j.ursetta@lmco.com>
2018-11-20 15:14:20 -0700

    Changed console logs to M.log.verbose()

--- scripts/test.js
+++ scripts/test.js
@@ -37,9 +37,9 @@ if (module.parent == null) {
  */
 function test(_args) {
   printHeader();
-  console.log('Running tests on mongo instance...'); // eslint-disable-line no-console
-  console.log(`Mongo IP: ${M.config.db.url}:${M.config.db.port}`);  // eslint-disable-line no-console
-  console.log(`Mongo DB: ${M.config.db.name}`);  // eslint-disable-line no-console
+  M.log.verbose('Running tests on mongo instance...');
+  M.log.verbose(`Mongo IP: ${M.config.db.url}:${M.config.db.port}`);
+  M.log.verbose(`Mongo DB: ${M.config.db.name}`);
 
   // Check if environment is production and NOT --force
   if (M.env.toLowerCase() === 'production' && !_args.includes('--force')) {
