d91e54f1121461277a8bdd711d31d33abca5ffa5
Leah De Laurell <leah.p.delaurell@lmco.com>
2019-02-21 17:06:02 -0700

    added some documentation to the ui

--- app/ui/react-components/elements/element-list.jsx
+++ app/ui/react-components/elements/element-list.jsx
@@ -19,43 +19,65 @@
  *
  * @description This renders the element tree in the project's page.
  */
+
+// React Modules
 import React, { Component } from 'react';
+
+// MBEE Modules
 import { getRequest } from "../helper-functions/getRequest";
+
+// MBEE JSX Modules
 import List from '../general-components/list/list.jsx';
 import ListItem from '../general-components/list/list-item.jsx';
 
+// Define ElementList Component
 class ElementList extends Component {
     constructor(props) {
+        // Initialize parent props
         super(props);
 
+        // Initialize state props
         this.state = {
             elementChildren: null
         }
 
+        // Bind component functions
         this.constructListItem = this.constructListItem.bind(this);
     }
 
+    // Define recursive get request for element tree
     constructListItem(item) {
+        // Create a new promise
         return new Promise((resolve, reject) => {
+            // Initialize url
             const url = this.props.url;
 
+            // Do a get request to the MBEE elements api
             getRequest(`${url}/branches/master/elements/${item}`)
                 .then(containedElement => {
+                    // Initialize variables
                     const promises = [];
                     const listItems = [];
 
+                    // Check if element has contained elements
                     if (containedElement.contains.length > 0) {
+                        // Loop through the contained elements
                         for (let i = 0; i < containedElement.contains.length; i++) {
+                            // Put the elements in this function again and to the promise array
                             promises.push(this.constructListItem(containedElement.contains[i])
                                 .then((listItem) => {
+                                    // Put the contained elements into list array
                                     listItems.push(listItem);
                                 })
+                                // If an error is caught, throw error
                                 .catch((err) => console.log(err))
                             )
                         }
 
+                        // Resolve the promises
                         Promise.all(promises)
                         .then(() => {
+                            // Return the element tree in html form
                             return resolve(
                                 <List>
                                     <ListItem element={containedElement}/>
@@ -65,11 +87,13 @@ class ElementList extends Component {
                                 </List>
                             );
                         })
+                        // If caught error, throw error
                         .catch(err => {
                             console.log(err);
                         })
                     }
                     else {
+                        // Return the element in html form
                         return resolve(
                             <List>
                                 <ListItem key={item} element={containedElement}/>
@@ -77,6 +101,7 @@ class ElementList extends Component {
                         );
                     }
                 })
+                // If caught error, throw error
                 .catch(err => {
                     console.log(err);
                     return reject(err);
@@ -85,24 +110,31 @@ class ElementList extends Component {
     }
 
     componentDidMount() {
+        // Initialize variables
         const element = this.props.element;
-
         const promises = [];
         const listItems = [];
 
+        // Loop through model element contains field
         for (let i = 0; i < element.contains.length; i++) {
+            // Push promises to an array with the element recursive function
             promises.push(this.constructListItem(element.contains[i])
                 .then((listItem) => {
+                    // push the list items to an array
                     listItems.push(listItem);
                 })
+                // If caught error, throw error
                 .catch((err) => console.log(err))
             )
         }
 
+        // Resolve the promises
         Promise.all(promises)
         .then(() => {
+            // Set the element tree to the state
             this.setState({ elementChildren: listItems})
         })
+        // If caught error, throw error
         .catch(err => {
             console.log(err);
         })
@@ -111,6 +143,7 @@ class ElementList extends Component {
 
 
     render() {
+        // Render the element tree
         return (
             <List className='guideline'>
                 {this.state.elementChildren}
@@ -120,5 +153,5 @@ class ElementList extends Component {
 }
 
 
-
+// Export the component
 export default ElementList
