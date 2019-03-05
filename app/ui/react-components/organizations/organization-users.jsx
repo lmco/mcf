e0439ece67a12384af70484ed07d170c130c152f
Leah De Laurell <leah.p.delaurell@lmco.com>
2019-03-05 15:07:41 -0700

    deleted from my develop

--- app/ui/react-components/organizations/organization-users.jsx
+++ app/ui/react-components/organizations/organization-users.jsx
@@ -21,8 +21,6 @@ import React from 'react';
 // MBEE Modules
 import ListItem from '../general-components/list/list-item.jsx';
 import List from '../general-components/list/list.jsx';
-import {Modal, ModalBody} from 'reactstrap';
-import UserRoleEdit from '../user/user-role-edit.jsx';
 
 // Define function
 function OrganizationUsers(props) {
@@ -37,13 +35,6 @@ function OrganizationUsers(props) {
 
     // Return org member list
     return (
-        <React.Fragment>
-            {/*Modal for creating a project*/}
-            <Modal isOpen={this.state.modalCreate} toggle={this.handleCreateToggle}>
-                <ModalBody>
-                    { (this.state.modalCreate) ? < /> : '' }
-                </ModalBody>
-            </Modal>
             <div id='view' className='org-users'>
                 <div className='project-list-header'>
                     <h2>Users</h2>
@@ -53,7 +44,6 @@ function OrganizationUsers(props) {
                     </List>
                 </div>
             </div>
-        </React.Fragment>
     )
 }
 
