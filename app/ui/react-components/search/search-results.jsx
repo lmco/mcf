5339fad8795f66bb276c3c95daa26668a983f438
Josh Kaplan <joshua.d.kaplan@lmco.com>
2019-03-06 19:55:02 -0500

    Added basic search functionality

---
+++ app/ui/react-components/search/search-results.jsx
@@ -0,0 +1,74 @@
+/**
+ * Classification: UNCLASSIFIED
+ *
+ * @module ui.react-components.search.search-results
+ *
+ * @copyright Copyright (C) 2018, Lockheed Martin Corporation
+ *
+ * @license LMPI - Lockheed Martin Proprietary Information
+ *
+ * @owner Josh Kaplan <joshua.d.kaplan@lmco.com>
+ *
+ * @author Josh Kaplan <joshua.d.kaplan@lmco.com>
+ *
+ * @description This renders the search page.
+ */
+
+import React, { Component } from 'react';
+
+class SearchResults extends Component {
+
+    constructor(props) {
+        // Initialize parent props
+        super(props);
+
+        this.state = {
+            results: props.results
+        };
+
+        // Bind component functions
+        //this.onChange = this.onChange.bind(this);
+        //this.doSearch = this.doSearch.bind(this);
+    }
+
+    render() {
+        // If no results yet, render empty div
+        if (!this.props.results) {
+            return (<div></div>);
+        }
+
+        // If empty search results
+        if (Array.isArray(this.props.results) && this.props.results.length === 0) {
+            return (<div>No search results found.</div>);
+        }
+
+        const results = this.props.results.map(result => {
+            let htmlResult = JSON.stringify(result, null, 4);
+            htmlResult = htmlResult.replace(/\n/g, '<br/>');
+            htmlResult = htmlResult.replace(/ /g, '&nbsp;');
+
+
+            /* NOTE:
+             *  This isn't really that dangerous since we sanitize all
+             *  input on the server side.
+             *
+             *  ~jdk
+             */
+            return (
+                <div className={'search-result'} key={result.id}>
+                    <div dangerouslySetInnerHTML={{__html: htmlResult}} >
+                    </div>
+                </div>
+            )
+        });
+
+        return (
+            <div className={'search-results'}>
+                {results}
+            </div>
+        )
+    }
+}
+
+// Export component
+export default SearchResults
--- app/ui/react-components/search/search.jsx
+++ app/ui/react-components/search/search.jsx
@@ -1,15 +1,15 @@
 /**
  * Classification: UNCLASSIFIED
  *
- * @module ui.react-components.search
+ * @module ui.react-components.search.search
  *
  * @copyright Copyright (C) 2018, Lockheed Martin Corporation
  *
  * @license LMPI - Lockheed Martin Proprietary Information
  *
- * @owner Leah De Laurell <leah.p.delaurell@lmco.com>
+ * @owner Josh Kaplan <joshua.d.kaplan@lmco.com>
  *
- * @author Leah De Laurell <leah.p.delaurell@lmco.com>
+ * @author Josh Kaplan <joshua.d.kaplan@lmco.com>
  *
  * @description This renders the search page.
  */
@@ -17,10 +17,11 @@
 
 // React Modules
 import React, { Component } from 'react';
-import {Input, Form, FormGroup} from 'reactstrap';
+import {Input, Form, FormGroup, Button, Row, Col} from 'reactstrap';
 
 // MBEE Modules
 import { ajaxRequest } from '../helper-functions/ajaxRequests.js';
+import SearchResults from '../search/search-results.jsx';
 
 class Search extends Component{
     constructor(props) {
@@ -28,11 +29,13 @@ class Search extends Component{
         super(props);
 
         this.state = {
-            query: ""
+            query: "",
+            results: null
         };
 
         // Bind component functions
         this.onChange = this.onChange.bind(this);
+        this.doSearch = this.doSearch.bind(this);
     }
 
     // Define change function
@@ -40,20 +43,56 @@ class Search extends Component{
         this.setState({ [event.target.name]: event.target.value});
     }
 
-    render(){
+    doSearch() {
+        // Build query URL
+        const oid = this.props.project.org;
+        const pid = this.props.project.id;
+        const url = `/api/orgs/${oid}/projects/${pid}/branches/master/elements/search`;
+
+        // Do ajax request
+        $.ajax({
+            method: "GET",
+            url: `${url}?query=${this.state.query}`
+        })
+        .done(data => {
+            this.setState({ results: data });
+        })
+        .fail(res => {
+            if (res.status === 404) {
+                this.setState({ results: []});
+            }
+        });
+    }
+
+    render() {
         return (
-            <div>
-                <Form>
-                    <FormGroup>
-                        <Input type="text"
-                               name="query"
-                               id="query"
-                               placeholder="Search"
-                               value={this.state.query || ""}
-                               onChange={this.onChange}
-                               />
-                    </FormGroup>
+            <div id={'search'}>
+                <Form className={'search-form'} inline>
+                    <Row form>
+                        <Col md={10} sm={6}>
+                            <FormGroup id={'search-input-form-group'} className={"mb-2 mr-sm-2 mb-sm-0"}>
+                                <Input type="text"
+                                       name="query"
+                                       id="query"
+                                       placeholder="Search"
+                                       value={this.state.query || ""}
+                                       onChange={this.onChange}
+                                       />
+                            </FormGroup>
+                        </Col>
+                        <Col md={2} sm={6}>
+                            <Button className='btn'
+                                    outline color="primary"
+                                    onClick={this.doSearch}>
+                                Search
+                            </Button>
+                        </Col>
+                    </Row>
                 </Form>
+
+                <div>
+                    <SearchResults results={this.state.results}/>
+                </div>
             </div>
         )
     }
