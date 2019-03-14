462aacc6dc99ef290940868ecca474b0bb4beffa
Bieber, Austin J <austin.j.bieber@lmco.com>
2019-03-14 11:43:33 -0600

    Added event emitters for all controllers

--- app/controllers/user-controller.js
+++ app/controllers/user-controller.js
@@ -392,8 +392,8 @@ function create(requestingUser, users, options) {
       // Set function-wide createdUsers;
       createdUsers = _createdUsers;
 
-      // Emit event that users were created
-      EventEmitter.emit('Users Created', createdUsers);
+      // Emit the event users-created
+      EventEmitter.emit('users-created', createdUsers);
 
       // Find the default organization
       return Organization.findOne({ _id: M.config.server.defaultOrganizationId });
@@ -685,7 +685,12 @@ function update(requestingUser, users, options) {
       return User.bulkWrite(bulkArray);
     })
     .then(() => User.find(searchQuery, fieldsString).populate(populateString))
-    .then((foundUpdatedUsers) => resolve(foundUpdatedUsers))
+    .then((foundUpdatedUsers) => {
+      // Emit the event users-updated
+      EventEmitter.emit('users-updated', foundUpdatedUsers);
+
+      return resolve(foundUpdatedUsers);
+    })
     .catch((error) => reject(M.CustomError.parseCustomError(error)));
   });
 }
@@ -822,8 +827,13 @@ function createOrReplace(requestingUser, users, options) {
       });
     })
     .then(() => User.deleteMany({ _id: foundUsers.map(u => u._id) }))
-    // Create the new users
-    .then(() => create(requestingUser, usersToLookup, options))
+    .then(() => {
+      // Emit the event users-deleted
+      EventEmitter.emit('users-deleted', foundUsers);
+
+      // Create the new users
+      return create(requestingUser, usersToLookup, options);
+    })
     .then((_createdUsers) => {
       createdUsers = _createdUsers;
 
@@ -986,7 +996,9 @@ function remove(requestingUser, users, options) {
     .then(() => User.deleteMany(searchQuery))
     // Return the deleted users
     .then(() => {
-      EventEmitter.emit('Users Deleted', foundUsers);
+      // Emit the event users-deleted
+      EventEmitter.emit('users-deleted', foundUsers);
+
       return resolve(foundUsernames);
     })
     .catch((error) => reject(M.CustomError.parseCustomError(error)));
