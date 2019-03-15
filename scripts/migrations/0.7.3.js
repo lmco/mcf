2e3d463d45e5a95c0072dc0328c806ece7db7f12
Leah De Laurell <leah.p.delaurell@lmco.com>
2019-03-15 15:21:41 -0600

    fixed the bug hopefully

--- scripts/migrations/0.7.3.js
+++ scripts/migrations/0.7.3.js
@@ -50,20 +50,12 @@ module.exports.down = function() {
  */
 module.exports.up = function() {
   return new Promise((resolve, reject) => {
-    // Get all indexes from the users data
-    mongoose.connection.db.collection('users').indexes()
-    .then((indexes) => {
-      const promises = [];
-      // Loop through the found indexes
-      indexes.forEach((index) => {
-        // If unique ID index exists, delete from users collection
-        if (index.name === 'username_1') {
-          promises.push(mongoose.connection.db.collection('users').dropIndex('username_1'));
-        }
-      });
-
-      // Return when all organization indexes have been dropped
-      return Promise.all(promises);
+    mongoose.connection.db.collections()
+    .then((existingCollections) => {
+      // If the users collection exists, run the helper function
+      if (existingCollections.includes('users')) {
+        return twoToThreeUserHelper();
+      }
     })
     // Get all documents from the server data
     .then(() => mongoose.connection.db.collection('server_data').find({}).toArray())
@@ -84,3 +76,28 @@ module.exports.up = function() {
     .catch((error) => reject(error));
   });
 };
+
+/**
+ * @description Helper function for 0.7.2 to 0.7.3 migration. Handles all
+ * updates to the users collection.
+ */
+function twoToThreeUserHelper() {
+  return new Promise((resolve, reject) => {
+    // Get all indexes from the users data
+    mongoose.connection.db.collection('users').indexes()
+    .then((indexes) => {
+      const promises = [];
+      // Loop through the found indexes
+      indexes.forEach((index) => {
+        // If unique ID index exists, delete from users collection
+        if (index.name === 'username_1') {
+          promises.push(mongoose.connection.db.collection('users').dropIndex('username_1'));
+        }
+      });
+
+      // Return when all organization indexes have been dropped
+      return Promise.all(promises);
+    })
+    .catch((error) => reject(error));
+  });
+}
