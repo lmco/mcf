548a01a3219703fcf47a47eb1a79f7db9b7a4d31
Josh Kaplan <joshua.d.kaplan@lmco.com>
2019-03-20 13:08:35 -0400

    References MBX-901: Cleaned up linter issues in search. Added browser and jquery environments to eslintrc.

--- app/ui/components/project-views/search/search.jsx
+++ app/ui/components/project-views/search/search.jsx
@@ -14,29 +14,32 @@
  * @description This renders the search page.
  */
 
+/* Modified ESLint rules for React. */
+/* eslint no-unused-vars: "warn" */
 
 // React Modules
 import React, { Component } from 'react';
-import {Input, Form, FormGroup, Button, Row, Col} from 'reactstrap';
+import { Input, Form, FormGroup, Button, Row, Col } from 'reactstrap';
 
 // MBEE Modules
 import SearchResults from './search-results.jsx';
 
-class Search extends Component{
+class Search extends Component {
+
   constructor(props) {
     // Initialize parent props
     super(props);
 
     // Parse the get parameters
-    let getParams = {};
-    let getParamsRaw = this.props.location.search.slice(1);
+    const getParams = {};
+    const getParamsRaw = this.props.location.search.slice(1);
     getParamsRaw.split('&').forEach(keyValuePair => {
-      let arr = keyValuePair.split('=');
+      const arr = keyValuePair.split('=');
       getParams[arr[0]] = arr[1];
     });
 
     this.state = {
-      query: getParams['query'] || null,
+      query: getParams.query || null,
       results: null
     };
 
@@ -53,7 +56,7 @@ class Search extends Component{
 
   // Define change function
   onChange(event) {
-    this.setState({ [event.target.name]: event.target.value});
+    this.setState({ [event.target.name]: event.target.value });
   }
 
   doSearch() {
@@ -64,7 +67,7 @@ class Search extends Component{
 
     // Do ajax request
     $.ajax({
-      method: "GET",
+      method: 'GET',
       url: `${url}?q=${this.state.query}`
     })
     .done(data => {
@@ -72,7 +75,7 @@ class Search extends Component{
     })
     .fail(res => {
       if (res.status === 404) {
-        this.setState({ results: []});
+        this.setState({ results: [] });
       }
     });
   }
@@ -83,12 +86,12 @@ class Search extends Component{
                 <Form id={'search-form'} className={'search-form'} inline>
                     <Row form>
                         <Col md={10} sm={8} xs={6}>
-                            <FormGroup id={'search-input-form-group'} className={"mb-2 mr-sm-2 mb-sm-0"}>
+                            <FormGroup id={'search-input-form-group'} className={'mb-2 mr-sm-2 mb-sm-0'}>
                                 <Input type="text"
                                        name="query"
                                        id="query"
                                        placeholder="Search"
-                                       value={this.state.query || ""}
+                                       value={this.state.query || ''}
                                        onChange={this.onChange}
                                        />
                             </FormGroup>
@@ -107,9 +110,10 @@ class Search extends Component{
                     <SearchResults results={this.state.results}/>
                 </div>
             </div>
-    )
+    );
   }
+
 }
 
 // Export component
-export default Search
+export default Search;
