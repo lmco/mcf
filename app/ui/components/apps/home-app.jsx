0523f1d33d7577085d3bcf88f9c9ea04fc53cdf8
Josh Kaplan <joshua.d.kaplan@lmco.com>
2019-03-20 10:03:27 -0400

    Home-app linter issues mostly cleaned up

--- app/ui/components/apps/home-app.jsx
+++ app/ui/components/apps/home-app.jsx
@@ -15,6 +15,10 @@
  * @description This renders the homepage.
  */
 
+/* Modified ESLint rules for React. */
+/* eslint no-unused-vars: "warn" */
+/* eslint no-undef: "warn" */
+
 // React Modules
 import React, { Component } from 'react';
 import { Button, Modal, ModalBody } from 'reactstrap';
@@ -30,6 +34,7 @@ import { ajaxRequest } from '../helper-functions/ajaxRequests.js';
 
 // Define HomePage Component
 class HomeApp extends Component {
+
   constructor(props) {
     // Initialize parent props
     super(props);
@@ -62,13 +67,14 @@ class HomeApp extends Component {
   componentDidMount() {
     const url = '/api/users/whoami';
 
+    // TODO (jk->ld) - We should be using jQuery rather than our own custom AJAX function
     ajaxRequest('GET', `${url}`)
     .then(user => {
       // Get the organization and their projects
-      ajaxRequest('GET', `/api/orgs?populate=projects`)
+      ajaxRequest('GET', '/api/orgs?populate=projects')
       .then(orgs => {
         // Set user state
-        this.setState({user: user});
+        this.setState({ user: user });
 
         // Add event listener for window resizing
         window.addEventListener('resize', this.handleResize);
@@ -80,9 +86,9 @@ class HomeApp extends Component {
         const allProjects = [];
 
         // Loop through orgs
-        orgs.map((org) => {
+        orgs.forEach((org) => {
           // Loop through projects and push to array
-          org.projects.map(project => {
+          org.projects.forEach(project => {
             allProjects.push(project);
           });
 
@@ -97,48 +103,27 @@ class HomeApp extends Component {
         });
 
         // Verify there are orgs
-        if(writePermOrgs.length > 0) {
+        if (writePermOrgs.length > 0) {
           // Set write states
-          this.setState({write: true});
-          this.setState({writePermOrgs: writePermOrgs});
+          this.setState({ write: true });
+          this.setState({ writePermOrgs: writePermOrgs });
         }
 
         // Verify user is admin
         if (user.admin) {
           // Set admin state
-          this.setState({admin: user.admin});
+          this.setState({ admin: user.admin });
         }
 
         // Set the org state
-        this.setState({orgs: orgs});
+        this.setState({ orgs: orgs });
 
         // Set the org state
-        this.setState({projects: allProjects});
-
-        let buffer,
-          latchId,
-          code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65, 13];
-
-        // Define event listener
-        const k = (ev) => {
-          buffer = buffer || code.slice();
-          if (buffer[0] === ev.keyCode) {
-            window.clearTimeout(latchId);
-            buffer.splice(0, 1);
-            if (buffer.length === 0) {
-              this.handleModalToggle();
-            }
-            latchId = window.setTimeout(function () {
-              buffer = code.slice();
-            }, 2000);
-          }
-        };
-
-        window.addEventListener("keyup", k);
+        this.setState({ projects: allProjects });
       })
       .catch(err => {
         // Throw error and set error state
-        this.setState({error: `Failed to grab orgs: ${err}`});
+        this.setState({ error: `Failed to grab orgs: ${err}` });
       });
     })
     .catch(err => {
@@ -154,7 +139,7 @@ class HomeApp extends Component {
   // Define resize functionality
   handleResize() {
     // Set state to width of window
-    this.setState({ width: this.ref.current.clientWidth })
+    this.setState({ width: this.ref.current.clientWidth });
   }
 
   // Define modal toggle functionality
@@ -180,11 +165,12 @@ class HomeApp extends Component {
     const list = this.state.orgs.map(org => {
       const username = this.state.user.username;
 
+      // TODO (jk->ld) - Discuss this one.
       if ((org.permissions[username] === 'write') || (org.permissions[username] === 'admin')) {
-        return( <OrgList org={org} write={this.state.write} admin={this.state.admin}/> )
+        return (<OrgList org={org} write={this.state.write} admin={this.state.admin}/>);
       }
       else {
-        return (<OrgList org={org} admin={this.state.admin}/>)
+        return (<OrgList org={org} admin={this.state.admin}/>);
       }
     });
 
@@ -196,26 +182,26 @@ class HomeApp extends Component {
                         <Space />
                     </ModalBody>
                 </Modal>
-                {/*Modal for creating an org*/}
+                { /* Modal for creating an org */ }
                 <Modal isOpen={this.state.modalCreate} toggle={this.handleCreateToggle}>
                     <ModalBody>
                         <Create toggle={this.handleCreateToggle}/>
                     </ModalBody>
                 </Modal>
-                {/*Modal for deleting an org*/}
+                { /* Modal for deleting an org */ }
                 <Modal isOpen={this.state.modalDelete} toggle={this.handleDeleteToggle}>
                     <ModalBody>
                         <Delete orgs={this.state.orgs} toggle={this.handleDeleteToggle}/>
                     </ModalBody>
                 </Modal>
-                {/*Display the list of projects*/}
+                { /* Display the list of projects */ }
                 <div id='view' className='org-list' ref={this.ref}>
                     <div className='org-list-header'>
                         <h2 className='org-header'>Organizations</h2>
-                        {/*Verify user is an admin */}
+                        { /* Verify user is an admin */ }
                         {(!this.state.admin)
                           ? ''
-                        // Display create and delete buttons
+                          // Display create and delete buttons
                           : (<div className='org-button'>
                                 <Button className='btn'
                                         outline color="secondary"
@@ -231,20 +217,17 @@ class HomeApp extends Component {
                         }
                     </div>
                     <hr/>
-                    {/*Verify there are projects*/}
+                    { /* Verify there are projects */ }
                     {(this.state.orgs.length === 0)
-                      ?(<div className='list-item'>
-                            <h3> No organizations. </h3>
-                        </div>)
-                      :(<List>
-                            {list}
-                        </List>)
+                      ? (<div className='list-item'><h3> No organizations.</h3></div>)
+                      : (<List>{list}</List>)
                     }
                 </div>
             </React.Fragment>
 
     );
   }
+
 }
 
 ReactDOM.render(<HomeApp />, document.getElementById('main'));
