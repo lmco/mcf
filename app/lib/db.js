9689bfdffcfd3f06d2fffca2f66539df26b5b098
Bieber, Austin J <austin.j.bieber@lmco.com>
2019-04-01 16:17:50 -0600

    Removed poolSize in db

--- app/lib/db.js
+++ app/lib/db.js
@@ -41,7 +41,7 @@ module.exports.connect = function() {
     }
     connectURL = `${connectURL + url}:${dbPort}/${dbName}`;
 
-    const options = { poolSize: 20 };
+    const options = {};
 
     // Configure an SSL connection
     // The 'sslCAFile' references file located in /certs.
