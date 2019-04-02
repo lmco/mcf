a82e7508db4d5702d8db0156791ac7036074bdbf
Leah De Laurell <leah.p.delaurell@lmco.com>
2019-04-02 08:53:39 -0600

    fixed a small issue but its ugly

--- app/ui/components/apps/nav.jsx
+++ app/ui/components/apps/nav.jsx
@@ -102,51 +102,51 @@ class MbeeNav extends Component {
 
   render() {
     return (
-      <div className='mbee-navbar' ref="navbar" style={{ width: '100%' }}>
-        <Navbar expand="md" style={{ height: '100%' }}>
+      <div ref='navbar' style={{ width: '100%' }}>
+        <Navbar className='mbee-navbar' dark expand='md'>
           { /* Create the MBEE Logo on navbar */ }
           <NavbarBrand href="/">
-            <img src="/img/logo-alt.png" />
+            <img src='/img/logo-alt.png' />
             { /* Change title based on width of window */ }
             {(this.state.width > 900) ? 'Model Based Engineering Environment' : 'MBEE'}
           </NavbarBrand>
           <NavbarToggler onClick={this.toggle} />
           <Collapse isOpen={this.state.isOpen} navbar>
-            <Nav className="ml-auto" navbar>
+            <Nav className='ml-auto' navbar>
               { /* Create links in navbar for documentation drop down */ }
               <UncontrolledDropdown nav inNavbar>
                 <DropdownToggle nav caret>
                   Docs
                 </DropdownToggle>
                 <DropdownMenu right>
-                  <DropdownItem href="/doc/flight-manual">
+                  <DropdownItem href='/doc/flight-manual'>
                     Flight Manual
                   </DropdownItem>
                   <DropdownItem divider />
-                  <DropdownItem href="/doc/developers">
+                  <DropdownItem href='/doc/developers'>
                     JSDoc Documentation
                   </DropdownItem>
-                  <DropdownItem href="/doc/api">
+                  <DropdownItem href='/doc/api'>
                     API Documentation
                   </DropdownItem>
                 </DropdownMenu>
               </UncontrolledDropdown>
               { // Create link to login or logout
                 (this.state.user === null)
-                  ? <NavLink href="/login">Login</NavLink>
+                  ? <NavLink href='/login'>Login</NavLink>
                   : (
                     <UncontrolledDropdown nav inNavbar>
                       <DropdownToggle nav caret>
                         <i className='fas fa-user-circle'/>
                       </DropdownToggle>
                       <DropdownMenu right>
-                        <DropdownItem href="/profile">Profile</DropdownItem>
+                        <DropdownItem href='/profile'>Profile</DropdownItem>
                         <DropdownItem divider />
-                        <DropdownItem href="/profile/orgs">Your Organizations</DropdownItem>
-                        <DropdownItem href="/profile/projects">Your Projects</DropdownItem>
-                        <DropdownItem href="/about">About</DropdownItem>
+                        <DropdownItem href='/profile/orgs'>Your Organizations</DropdownItem>
+                        <DropdownItem href='/profile/projects'>Your Projects</DropdownItem>
+                        <DropdownItem href='/about'>About</DropdownItem>
                         <DropdownItem divider />
-                        <DropdownItem><NavLink href="/logout">Log Out</NavLink></DropdownItem>
+                        <DropdownItem><NavLink href='/logout'>Log Out</NavLink></DropdownItem>
                       </DropdownMenu>
                     </UncontrolledDropdown>
                   )
