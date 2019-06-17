84b7df33b43a726b2c37d0bf8c6466c76ad35573
Connor Doyle <connor.p.doyle@lmco.com>
2019-06-17 11:28:40 -0600

    gzip is no longer a param

--- app/api-routes.js
+++ app/api-routes.js
@@ -2779,11 +2779,6 @@ api.route('/orgs/:orgid/projects/:projectid/branches/:branchid/elements/search')
  *         in: query
  *         type: boolean
  *         default: false
- *       - name: gzip
- *         description: If true, will process an uploaded gzip file.
- *         in: query
- *         type: boolean
- *         default: false
  *     responses:
  *       200:
  *         description: OK, Succeeded to POST elements, return element public
