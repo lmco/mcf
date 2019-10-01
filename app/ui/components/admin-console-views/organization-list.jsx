806a6840857ec82342edb24023071439b9c6372b
Leah De Laurell <leah.p.delaurell@lmco.com>
2019-10-01 15:21:44 -0600

    updated the admin console

--- app/ui/components/admin-console-views/organization-list.jsx
+++ app/ui/components/admin-console-views/organization-list.jsx
@@ -41,7 +41,6 @@ class OrganizationList extends Component {
     this.state = {
       width: null,
       orgs: [],
-      admin: false,
       modalCreate: false,
       modalDelete: false,
       error: null
@@ -77,12 +76,6 @@ class OrganizationList extends Component {
           url: `${base}?${opt}`,
           statusCode: {
             200: (orgs) => {
-              // Verify if admin user
-              if (data.admin) {
-                // Set admin state
-                this.setState({ admin: data.admin });
-              }
-
               // Set org state
               this.setState({ orgs: orgs });
 
@@ -154,29 +147,24 @@ class OrganizationList extends Component {
             <h2 className='workspace-title workspace-title-padding'>
               Organizations
             </h2>
-              {/* Verify user is an admin */}
-              {(!this.state.admin)
-                ? ''
-                // Display create and delete buttons
-                : (<div className='workspace-header-button'>
-                    <Button className='btn'
-                            outline color="secondary"
-                            onClick={this.handleCreateToggle}>
-                      {(this.state.width > 600)
-                        ? 'Create'
-                        : (<i className='fas fa-plus add-btn'/>)
-                      }
-                    </Button>
-                    <Button className='btn'
-                            outline color="danger"
-                            onClick={this.handleDeleteToggle}>
-                      {(this.state.width > 600)
-                        ? 'Delete'
-                        : (<i className='fas fa-trash-alt delete-btn'/>)
-                      }
-                    </Button>
-                  </div>)
-              }
+              <div className='workspace-header-button'>
+                <Button className='btn'
+                        outline color="secondary"
+                        onClick={this.handleCreateToggle}>
+                  {(this.state.width > 600)
+                    ? 'Create'
+                    : (<i className='fas fa-plus add-btn'/>)
+                  }
+                </Button>
+                <Button className='btn'
+                        outline color="danger"
+                        onClick={this.handleDeleteToggle}>
+                  {(this.state.width > 600)
+                    ? 'Delete'
+                    : (<i className='fas fa-trash-alt delete-btn'/>)
+                  }
+                </Button>
+              </div>
           </div>
           {/* Verify there are orgs */}
           <div id='workspace-body' className='extra-padding'>
