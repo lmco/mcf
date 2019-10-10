1c0970079ce230aab5d361b0299ba6b0375c4dbe
Lee, Phillip <phillip.lee@lmco.com>
2019-10-10 15:09:15 +0000

    Apply suggestion to app/models/artifact.js

--- app/models/artifact.js
+++ app/models/artifact.js
@@ -14,7 +14,7 @@
  * @author Phillip Lee <phillip.lee@lmco.com>
  *
  * @description
- * <p>This module defines the artifact data model. Artifact data model
+ * <p>This module defines the artifact data model. The artifact data model
  * represent metadata for Artifact blobs (binary large object).
  * Filename and Location are two examples of Artifact metadata.
  * They are used to point and locate artifact blobs.
