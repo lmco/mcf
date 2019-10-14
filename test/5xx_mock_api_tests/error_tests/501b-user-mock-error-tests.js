2eeff0ac260a40027f1284d5e786175015631c4a
Connor Doyle <connor.p.doyle@lmco.com>
2019-10-14 12:07:17 -0600

    fixed description

--- test/5xx_mock_api_tests/error_tests/501b-user-mock-error-tests.js
+++ test/5xx_mock_api_tests/error_tests/501b-user-mock-error-tests.js
@@ -1,7 +1,7 @@
 /**
  * @classification UNCLASSIFIED
  *
- * @module test.501a-user-mock-core-tests
+ * @module test.501b-user-mock-core-tests
  *
  * @copyright Copyright (C) 2018, Lockheed Martin Corporation
  *
