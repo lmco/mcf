b7ab585d45865751f4b08c409ef1e86e7d46c513
Connor Doyle <connor.p.doyle@lmco.com>
2019-12-19 16:06:28 -0700

    create the security file on startup

--- app/lib/logger.js
+++ app/lib/logger.js
@@ -211,6 +211,10 @@ function makeLogger(subcommand, opts) {
 // Add defined colors to winston logger
 winston.addColors(colors);
 
+// Create the security log if it doesn't already exist
+const securityPath = path.join('logs', M.config.log.security_file);
+if (!fs.existsSync(securityPath)) fs.appendFileSync(securityPath, '');
+
 /**
  * @description Logs the response to an HTTP request.
  *
