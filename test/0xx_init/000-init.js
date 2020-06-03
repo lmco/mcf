a9c8f3fa89be98272f6bce002c3fa0102ec96511
McDaniel, Donte S <donte.s.mcdaniel@lmco.com>
2020-06-03 14:57:51 -0400

    Updated

--- test/0xx_init/000-init.js
+++ test/0xx_init/000-init.js
@@ -42,6 +42,7 @@ const ArtifactStrategy = M.require(`artifact.${M.config.artifact.strategy}`);
  * name of the current file.
  */
 describe(M.getModuleName(module.filename), function() {
+  this.timeout(30000);
   /**
    * Execute the tests.
    */
