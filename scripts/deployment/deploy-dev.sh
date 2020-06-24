e1982394ebe6cc52a8a623e24f72df8758d0941e
McDaniel, Donte S <donte.s.mcdaniel@lmco.com>
2020-06-24 11:13:03 -0400

    Updated Cluster

--- scripts/deployment/deploy-dev.sh
+++ scripts/deployment/deploy-dev.sh
@@ -4,23 +4,27 @@ ProjectDirectory=$1
 Version=$2
 
 # Create dev namespace
-echo 'Creating the dev namespace if it does not exist'
+echo '### Creating the dev namespace if it does not exist ###'
 kubectl create namespace dev
 
 # Create secret for accessing gitlab docker registry
-echo 'Creating the secret to access gitlab docker registry if it does not exist'
+echo '### Creating the secret to access gitlab docker registry if it does not exist ###'
 kubectl create secret docker-registry mcf-repo --namespace=dev --docker-server=registry.gitlab.us.lmco.com:443 --docker-username=gitlab+deploy-token-832 --docker-password=q12LwJwfpNBzRyTYTcNj
 
+# Create persistent volume
+echo '### Creating Persistent Volume ###'
+kubectl create -f $ProjectDirectory/kubernetes/mcf/mcf-dev-pv.yaml -n dev
+
 # Removing old mcf deployment pod. Scales replicas to 0
-echo 'Scaling the deployment to 0' 
+echo '### Scaling the deployment to 0 ###' 
 kubectl scale --replicas=0 deployment mcf-deployment -n dev
 
 # Create the mcf deployment
-echo 'Creating the mcf deployment if it does not exists'
+echo '### Creating the mcf deployment if it does not exists ###'
 kubectl create -f $ProjectDirectory/kubernetes/mcf/mcf-dev-deployment.yaml -n dev
 
 # Updating the replicas to 1.
-echo 'Updating the replicas to 1'
+echo '### Updating the replicas to 1 ###'
 kubectl scale --replicas=1 deployment mcf-deployment -n dev
 
 exit 0
