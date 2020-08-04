c76fbe619ace5d105ad7ed1aacea33f9cd1e9d7c
Connor Doyle <connor.p.doyle@lmco.com>
2020-08-03 19:21:33 -0600

    fixed bug where singular api calls were crashing due to middleware

--- app/lib/middleware.js
+++ app/lib/middleware.js
@@ -162,28 +162,28 @@ function disableUserPatchPassword(req, res, next) { // eslint-disable-line consi
 function pluginPre(endpoint) {
   if (M.config.server.plugins.enabled) {
     // eslint-disable-next-line global-require
-    const { pluginFunctions } = require(path.join(M.root, 'plugins', 'routes.js'));
+    const {pluginFunctions} = require(path.join(M.root, 'plugins', 'routes.js'));
 
-    return async function(req, res, next) {
-      try {
-        for (let i = 0; i < pluginFunctions[endpoint].pre.length; i++) {
-          // eslint-disable-next-line no-await-in-loop
-          await pluginFunctions[endpoint].pre[i](req, res);
+    if (pluginFunctions[endpoint]) {
+      return async function(req, res, next) {
+        try {
+          for (let i = 0; i < pluginFunctions[endpoint].pre.length; i++) {
+            // eslint-disable-next-line no-await-in-loop
+            await pluginFunctions[endpoint].pre[i](req, res);
+          }
+          next();
         }
-        next();
-      }
-      catch (error) {
-        M.log.warn(error);
-        const statusCode = errors.getStatusCode(error);
-        utils.formatResponse(req, res, error.message, statusCode, next);
-      }
-    };
+        catch (error) {
+          M.log.warn(error);
+          const statusCode = errors.getStatusCode(error);
+          utils.formatResponse(req, res, error.message, statusCode, next);
+        }
+      };
+    }
   }
-  else {
-    return function(req, res, next) {
-      next();
-    };
-  }
+  return function(req, res, next) {
+    next();
+  };
 }
 
 /**
