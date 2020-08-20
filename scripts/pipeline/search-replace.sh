c9e824152f11253d32f508fb18317dd10c5d53fc
McDaniel, Donte S <donte.s.mcdaniel@lmco.com>
2020-08-20 12:28:28 -0400

    Adding search replace script

--- scripts/pipeline/search-replace.sh
+++ scripts/pipeline/search-replace.sh
@@ -2,20 +2,21 @@
 
 # This script searches and replaces all sensitive data with gitlab protected variables
 
-NFS_SHARE_URL=$1
-DOCKER_PASSWORD=$2
+CI_PROJECT_DIR=$1
+NFS_SHARE_URL=$2
+DOCKER_PASSWORD=$3
 
 ### Replace nfs_share_address in dev and prod persistant volume config with gitlab protected variable value
-sed -i "s/server: nfs_share_address/server: $NFS_SHARE_URL/" ../../kubernetes/mcf/mcf-dev-pv.yaml
-sed -i "s/server: nfs_share_address/server: $NFS_SHARE_URL/" ../../kubernetes/mcf/mcf-prod-pv.yaml
+sed -i "s/server: nfs_share_address/server: $NFS_SHARE_URL/" $CI_PROJECT_DIR/kubernetes/mcf/mcf-dev-pv.yaml
+sed -i "s/server: nfs_share_address/server: $NFS_SHARE_URL/" $CI_PROJECT_DIR/kubernetes/mcf/mcf-prod-pv.yaml
 
 ### Replace docker password in all deploy scripts with gitlab protected variable value
-sed -i "s/--docker-password=dockerpassword/--docker-password=$DOCKER_PASSWORD/" ../deployment/deploy-dev.sh
-sed -i "s/--docker-password=dockerpassword/--docker-password=$DOCKER_PASSWORD/" ../deployment/deploy-prod.sh
-sed -i "s/--docker-password=dockerpassword/--docker-password=$DOCKER_PASSWORD/" ../deployment/deploy-test.sh
+sed -i "s/--docker-password=dockerpassword/--docker-password=$DOCKER_PASSWORD/" $CI_PROJECT_DIR/scripts/deployment/deploy-dev.sh
+sed -i "s/--docker-password=dockerpassword/--docker-password=$DOCKER_PASSWORD/" $CI_PROJECT_DIR/scripts/deployment/deploy-prod.sh
+sed -i "s/--docker-password=dockerpassword/--docker-password=$DOCKER_PASSWORD/" $CI_PROJECT_DIR/scripts/deployment/deploy-test.sh
 
-cat ../../kubernetes/mcf/mcf-dev-pv.yaml
-cat ../../kubernetes/mcf/mcf-prod-pv.yaml
-cat ../deployment/deploy-dev.sh
-cat ../deployment/deploy-prod.sh
-cat ../deployment/deploy-test.sh
+cat $CI_PROJECT_DIR/kubernetes/mcf/mcf-dev-pv.yaml
+cat $CI_PROJECT_DIR/kubernetes/mcf/mcf-prod-pv.yaml
+cat $CI_PROJECT_DIR/scripts/deployment/deploy-dev.sh
+cat $CI_PROJECT_DIR/scripts/deployment/deploy-prod.sh
+cat $CI_PROJECT_DIR/scripts/deployment/deploy-test.sh
