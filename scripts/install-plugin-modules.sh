cb06b1dc08f8367fe1b03b9df4c3c53ad3fa7344
McDaniel, Donte S <donte.s.mcdaniel@lmco.com>
2020-11-23 16:55:34 -0500

    Updated plugin module script

--- scripts/install-plugin-modules.sh
+++ scripts/install-plugin-modules.sh
@@ -3,8 +3,8 @@
 for D in `find all_plugins -type d -maxdepth 1`
 do
     # echo $D
-    if [ $D != "plugins" ]; then
+    if [ $D != "all_plugins" ]; then
         echo $D
-        (cd $D && yarn install)        
+        (cd $D && yarn install)
     fi
 done
